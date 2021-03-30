3f60b3a135682e4cc11b6cdca8dada5c35387c39
TahaElmi <elmi.taha@gmail.com>
2021-03-30 17:45:22 +0430

    Initial commit

---
+++ Assets/Scripts/ChefController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChefController : MonoBehaviour
+{
+    public float speed;
+    public float fastRatio;
+    
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D) && transform.position.x < 8.5)
+        {
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                transform.Translate(speed * fastRatio, 0, 0);
+
+            }
+            else
+            {
+                transform.Translate(speed, 0, 0);
+
+            }
+        }
+        
+        if (Input.GetKey(KeyCode.A) && transform.position.x > -8.5)
+        {
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                transform.Translate(-speed * fastRatio, 0, 0);
+            }
+            else
+            {
+                transform.Translate(-speed, 0, 0);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/EggFall.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+using Random = UnityEngine.Random;
+
+public class EggFall : MonoBehaviour
+{
+    private int eggNumber;
+    private GameObject egg;
+    private GameObject parrentEgg;
+    private float delayTime;
+
+    private void ActiveEgg()
+    {
+        eggNumber = Random.Range(0, 56);
+        String eggName = "Egg (" + eggNumber + ")";
+        parrentEgg = GameObject.Find("Eggs");
+        
+        Transform[] trs= parrentEgg.GetComponentsInChildren<Transform>(true);
+        foreach(Transform t in trs){
+            if(t.name == eggName){
+                egg = t.gameObject;
+            }
+        }
+        
+        if (egg)
+        {
+            egg.SetActive(true);
+            Debug.Log(eggName);
+        }
+        else
+        {
+            Debug.Log("Nope:" + eggName);
+        }
+    }
+
+    private void Update()
+    {
+        delayTime = 2;
+        ActiveEgg();
+        while (delayTime != 0)
+        {
+            delayTime -= Time.deltaTime;
+        }
+    }
+}
